385918ed08c06ee3ddb9b1d339dc0e1d71e901de
Hendrik Schneider <hendrikschneider@users.noreply.github.com>
2017-08-06 13:52:06 +0300

    Revert "Fix piwik.js, so it matches documentation. …"

--- test/PiwikTest.rb
+++ test/PiwikTest.rb
@@ -18,11 +18,11 @@ class PiwikTest < Test::Unit::TestCase
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
     (function() {
-    var u=\'//\'+\"my.piwik.server/path\";
+    var u=\"my.piwik.server/path\";
     _paq.push(['setTrackerUrl', u+'/piwik.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'/piwik.js'; s.parentNode.insertBefore(g,s);
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
     })();
     </script>
     <!-- End Piwik Code -->
