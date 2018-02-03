db3229c53f79ca28ae39f59b3b80e770099b8d35
Hendrik Schneider <hendrikschneider@users.noreply.github.com>
2018-02-03 21:10:45 +0000

    Update Matomo.rb

--- lib/analytics/Matomo.rb
+++ lib/analytics/Matomo.rb
@@ -6,11 +6,11 @@ class Matomo
       _paq.push(['trackPageView']);
       _paq.push(['enableLinkTracking']);
       (function() {
-        var u='//'+\"%{url}/\";
+        var u='//'+\"%{url}\";
         _paq.push(['setTrackerUrl', u+'piwik.php']);
         _paq.push(['setSiteId', '%{siteId}']);
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'/piwik.js'; s.parentNode.insertBefore(g,s);
       })();
     </script>
     <!-- End Matomo Code -->
